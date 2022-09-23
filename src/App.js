5803058fd6b4878ab7e295ee114c387d66fa5ef9
Kostiantyn Pavlov <realanka77@gmail.com>
2022-09-23 20:04:40 +0300

    feat: add new components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
+import Lesson from './Lesson';
 import React, { Component, Fragment } from 'react';
 import PropTypes from "prop-types";
 import { Counter, CounterButton, Button } from "./Components/ls3";
@@ -8,7 +9,7 @@ import { Counter, CounterButton, Button } from "./Components/ls3";
 
 
 const App = () => (
-  <Form></Form>
+  <Lesson></Lesson>
 )
 
 export default App;
---
+++ src/Lesson.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {BrowserRouter as Router, Link } from "react-router-dom";
+
+const AppLink = (props) => ({
+  render: () => (
+    <Link {...props} activeClassName="active"></Link>
+  ),
+});
+
+class Lesson extends Component {
+   render() {
+      return (
+         <Router>
+            <nav>
+               <AppLink to="/" >Home</AppLink>
+               <AppLink to="/portfolio" >Portfolio</AppLink>
+               <AppLink to="/contacts" >Contacts</AppLink>
+            </nav>
+         </Router>
+      );
+   }
+}
+
+export default Lesson;
